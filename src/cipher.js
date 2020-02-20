a94ee8976e9fe66d691b17016d123bc935441428
Carranza94 <jgca9417@gmail.com>
2020-02-20 11:57:03 -0500

    agregando archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,53 @@
-const cipher = {
-  // ...
+const cipher = {//Creo un objeto porque me permitirá almacenar las funciones que ultilizaré para reutilizar mi cifrado y decifrado
+    encode:(palabra1,offSetC) => {
+        let resultado = '';  //Creo la variable resultado vacío para almacenar ahí los múltiples resultado de cada condición 
+        for(let i= 0; i < palabra1.length; i++)
+        {
+          let positionAsciiEncode = palabra1.charCodeAt(i);// Utilizo el charCodeAt() para conocer la posición del dígito que me dan, dependiendo de su posición se ejecutará de diferente
+          if (positionAsciiEncode >= 65 && positionAsciiEncode <= 90){
+            let encodeMayus = String.fromCharCode((positionAsciiEncode - 65 + parseInt(offSetC))%26 + 65);
+            resultado = resultado + encodeMayus;
+          } else if(positionAsciiEncode >= 97 && positionAsciiEncode <=122){
+            let encondeMinusculas = String.fromCharCode((positionAsciiEncode - 97 + parseInt(offSetC))%26 + 97);
+            resultado = resultado + encondeMinusculas;
+          } else if(positionAsciiEncode >= 32 && positionAsciiEncode <= 64){
+            let especialE = String.fromCharCode((positionAsciiEncode - 32 + parseInt(offSetC))%33 + 32);
+            resultado = resultado + especialE;
+          } 
+        } 
+        return resultado// antes de finalizar una función, en la ultima penúltima línea debo explicar a qué me va a dar como resultado 
+      },// No olvidar que las propiedades son separadas por comas simples y no por punto y coma. 
+      decode:(palabra2,offSetD) => {
+        let resultado2= '';
+        for(let i= 0; i < palabra2.length; i++)
+        {
+          let positionAsciiDecode = palabra2.charCodeAt(i);
+          if(positionAsciiDecode >= 65 && positionAsciiDecode <= 90)
+          {
+            let decodeMayus = String.fromCharCode((positionAsciiDecode - 90 - parseInt(offSetD))%26 + 90);
+            resultado2 = resultado2 + decodeMayus;
+            string1.value = resultado2;
+          } else if(positionAsciiDecode >= 97 && positionAsciiDecode <= 122){
+            let decodeMinusculas = String.fromCharCode((positionAsciiDecode - 122 - parseInt(offSetD))%26 + 122);
+            resultado2 = resultado2 + decodeMinusculas;
+            string1.value = resultado2;
+          } else if(positionAsciiDecode >= 32 && positionAsciiDecode <= 64){
+            let especialD = String.fromCharCode((positionAsciiDecode - 64 - parseInt(offSetD))%33 + 64);
+            resultado2 = resultado2 + especialD;
+            string1.value = resultado2;
+          }
+        }
+        return resultado2
+      }
 };
 
 export default cipher;
+
+
+
+
+
+//export default decipher;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+/*console.log(cipher);*/
+
+let string1 = document.getElementById('string1');
+let string2 = document.getElementById('string2');
+let click1 = document.getElementById('click1');
+let click2 = document.getElementById('click2');
+let offSet1 = document.getElementById('offset1');
+let offSet2 = document.getElementById('offset2');
+
+
+click1.addEventListener('click',() => {
+  let palabra1 = string1.value;
+  //console.log(palabra1);
+  let offSetC = offSet1.value;
+  string2.value = cipher.encode(palabra1,offSetC);
+});  
+
+
+click2.addEventListener('click',() =>
+{
+  let palabra2 = string2.value;
+  let offSetD = offSet2.value;
+  string1.value = cipher.decode(palabra2,offSetD);
+}); 
+
+
+
+/*else if(positionAsciiEncode === 33){
+      resultado += '!';
+      string2.value = resultado;
+    } else if(positionAsciiEncode === 64){
+      resultado += '@';
+      string2.value = resultado;
+    } else if(positionAsciiEncode === 32){
+      resultado += ' ';
+      string2.value = resultado;
+    }*/ 
+/*else if(positionAsciiDecode === 33){
+      resultado2 += '!';
+      string1.value = resultado2;
+    } else if(positionAsciiDecode === 64){
+      resultado2 += '@';
+      string1.value = resultado2;
+    } else if(positionAsciiDecode === 32){
+      resultado2 += ' ';
+      string1.value = resultado2;
+    }*/ 
+    
+/*
+for(let i= 0; i < palabra.length; i++){
+
+  let encode = String.fromCharCode((palabra.charCodeAt(i) - 65 + 5)%26 + 65);
+
+  resultado = resultado + encode;
+  
+}
+console.log(resultado);
+
+let palabra2 = 'MTQFHTRTJXYFX';
+let offset2 = 5;
+let resultado2 = '';
+
+for(let i= 0; i < palabra2.length; i++){
+
+  let decode = String.fromCharCode((palabra2.charCodeAt(i) - 90 - 5)%26 + 90);
+
+  resultado2 = resultado2 + decode;
+}
+
+console.log(resultado2);
+*/
+
+/*click1.addEventListener('click',() =>{
+    let mensaje1 = string1.value;
+    let desplazamiento = offSet1.value;
+    string2.value= mensaje1;
+});*/
