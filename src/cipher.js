32fffa6f18b4fc211841eed278b9b8b8484061c0
Carranza94 <jgca9417@gmail.com>
2020-02-21 15:21:43 -0500

    prueba test

--- src/cipher.js
+++ src/cipher.js
@@ -1,61 +1,63 @@
 const cipher = {//Creo un objeto porque me permitirá almacenar las funciones que ultilizaré para reutilizar mi cifrado y decifrado
     //Utilizaré el nombre encode para la propiedad que contiene la función que va a cifrar.
-    encode:(palabra1,offSetC) => {
-        let resultado = '';  //Creo la variable cuyo valor es vacio para almacenar ahí los múltiples resultado de cada condición 
-        for(let i= 0; i < palabra1.length; i++){//For para iterar
-          let positionAsciiEncode = palabra1.charCodeAt(i);// Utilizo el charCodeAt() para conocer la posición del dígito que me dan, dependiendo de su posición se ejecutará
-     //Los if's que están a continuación se emplearán solo sí el offset es mayor que 0     
-          if (positionAsciiEncode >= 65 && positionAsciiEncode <= 90 && offSetC>0){
-            let encodeMayusculas = String.fromCharCode((positionAsciiEncode - 65 + parseInt(offSetC))%26 + 65);
-            resultado = resultado + encodeMayusculas;
-          } else if(positionAsciiEncode >= 97 && positionAsciiEncode <=122 && offSetC>0){
-            let encondeMinusculas = String.fromCharCode((positionAsciiEncode - 97 + parseInt(offSetC))%26 + 97);
-            resultado = resultado + encondeMinusculas;
-          } else if(positionAsciiEncode >= 32 && positionAsciiEncode <= 64 && offSetC>0){
-            let encodeEspecial = String.fromCharCode((positionAsciiEncode - 32 + parseInt(offSetC))%33 + 32);
-            resultado = resultado + encodeEspecial;
-      //Tras la siguiente línea estarán los if's menores que 0     
-          } else if (positionAsciiEncode >= 65 && positionAsciiEncode <= 90 && offSetC<0){
-            let encodeMayusculasNegativo = String.fromCharCode((positionAsciiEncode - 90 + parseInt(offSetC))%26 + 90);
-            resultado = resultado + encodeMayusculasNegativo;
-          } else if(positionAsciiEncode >= 97 && positionAsciiEncode <=122 && offSetC<0){
-            let encondeMinusculasNegativo = String.fromCharCode((positionAsciiEncode - 122 + parseInt(offSetC))%26 + 122);
-            resultado = resultado + encondeMinusculasNegativo;
-          } else if(positionAsciiEncode >= 32 && positionAsciiEncode <= 64 && offSetC<0){
-            let encodeEspecialNegativo = String.fromCharCode((positionAsciiEncode - 64 + parseInt(offSetC))%33 + 64);
-            resultado = resultado + encodeEspecialNegativo;
-          }
-        } 
-        return resultado// antes de finalizar una función, en la ultima penúltima línea debo explicar a qué me va a dar como resultado 
-      },// No olvidar que las propiedades son separadas por comas simples y no por punto y coma. 
-    //Utilizaré el nombre decode para la propiedad que contiene la función que va a decifrar.
-      decode:(palabra2,offSetD) => {
-        let resultado2= '';//Variable vacía para almacenar resultado del algoritmo
-        for(let i= 0; i < palabra2.length; i++){//For para iterar
-          let positionAsciiDecode = palabra2.charCodeAt(i);//Posición en ASCII.
-          if(positionAsciiDecode >= 65 && positionAsciiDecode <= 90 && offSetD>0){//Condicional para Mayúsculas.
-            let decodeMayus = String.fromCharCode((positionAsciiDecode - 90 - parseInt(offSetD))%26 + 90);//Impresión del nuevo string ASCII a través del ALGORITMO
-            resultado2 = resultado2 + decodeMayus;
-          } else if(positionAsciiDecode >= 97 && positionAsciiDecode <= 122 && offSetD>0){//Condicional para Minúsculas.
-            let decodeMinus = String.fromCharCode((positionAsciiDecode - 122 - parseInt(offSetD))%26 + 122);//Impresión del nuevo string ASCII a través del ALGORITMO
-            resultado2 = resultado2 + decodeMinus;
-          } else if(positionAsciiDecode >= 32 && positionAsciiDecode <= 64 && offSetD>0){//Condicional para caracteres especiales.
-            let decodeEspecial = String.fromCharCode((positionAsciiDecode - 64 - parseInt(offSetD))%33 + 64);//Impresión del nuevo string ASCII a través del ALGORITMO
-            resultado2 = resultado2 + decodeEspecial;
-      //if's menores que 0     
-          } else if (positionAsciiDecode >= 65 && positionAsciiDecode <= 90 && offSetD<0){
-            let decodeMayusculasNegativo = String.fromCharCode((positionAsciiDecode - 65 - parseInt(offSetD))%26 + 65);
-            resultado2 = resultado2 + decodeMayusculasNegativo;
-          } else if(positionAsciiDecode >= 97 && positionAsciiDecode <=122 && offSetD<0){
-            let decondeMinusculasNegativo = String.fromCharCode((positionAsciiDecode - 97 - parseInt(offSetD))%26 + 97);
-            resultado2 = resultado2 + decondeMinusculasNegativo;
-          } else if(positionAsciiDecode >= 32 && positionAsciiDecode <= 64 && offSetD<0){
-            let decodeEspecialNegativo = String.fromCharCode((positionAsciiDecode - 32 - parseInt(offSetD))%33 + 32);
-            resultado2 = resultado2 + decodeEspecialNegativo;
-          }
+  encode:(offSetC,palabra1) => {
+    //Creo la variable cuyo valor es vacio para almacenar ahí los múltiples resultado de cada condición 
+    let cifrado = '';
+    if(offSetC === null || offSetC === 0){
+      throw new TypeError();
+    }
+    for(let i= 0; i < palabra1.length; i++){//For para iterar
+      const positionAsciiEncode = palabra1.charCodeAt(i); 
+      if (positionAsciiEncode >= 65 && positionAsciiEncode <= 90) {
+        if(offSetC > 0) {
+          cifrado += String.fromCharCode((positionAsciiEncode - 65 + parseInt(offSetC))%26 + 65);  
+        } else {
+          cifrado += String.fromCharCode((positionAsciiEncode - 90 + parseInt(offSetC))%26 + 90);
         }
-        return resultado2
+      } else if(positionAsciiEncode >= 97 && positionAsciiEncode <=122) {
+        if (offSetC > 0){
+          cifrado += String.fromCharCode((positionAsciiEncode - 97 + parseInt(offSetC))%26 + 97);
+        } else {
+          cifrado += String.fromCharCode((positionAsciiEncode - 122 + parseInt(offSetC))%26 + 122)
+        }
+      } else {
+        if (offSetC > 0){
+          cifrado += String.fromCharCode((positionAsciiEncode - 32 + parseInt(offSetC))%33 + 32);
+        } else {
+          cifrado += String.fromCharCode((positionAsciiEncode - 64 + parseInt(offSetC))%33 + 64);  
+        }
       }
+    }
+    return cifrado// antes de finalizar una función, en la ultima penúltima línea debo explicar a qué me va a dar como resultado 
+  },
+  decode:(offSetD,palabra2) => {
+    let decifrado= '';//Variable vacía para almacenar resultado del algoritmo
+    if(offSetD === null || offSetD === 0){
+      throw new TypeError();
+    }
+    for(let i= 0; i < palabra2.length; i++){//For para iterar
+      const positionAsciiDecode = palabra2.charCodeAt(i);//Posición en ASCII.
+      if(positionAsciiDecode >= 65 && positionAsciiDecode <= 90){
+        if(offSetD > 0){
+          decifrado += String.fromCharCode((positionAsciiDecode - 90 - parseInt(offSetD))%26 + 90);//Impresión del nuevo string ASCII a través del ALGORITMO 
+        } else{
+          decifrado += String.fromCharCode((positionAsciiDecode - 65 - parseInt(offSetD))%26 + 65);
+        } 
+      }else if(positionAsciiDecode >= 97 && positionAsciiDecode <= 122){
+        if(offSetD > 0){
+          decifrado += String.fromCharCode((positionAsciiDecode - 122 - parseInt(offSetD))%26 + 122);//Impresión del nuevo string ASCII a través del ALGORITMO 
+        }else {
+          decifrado += String.fromCharCode((positionAsciiDecode - 97 - parseInt(offSetD))%26 + 97);
+        }
+      }else {
+        if(offSetD > 0){
+          decifrado += String.fromCharCode((positionAsciiDecode - 64 - parseInt(offSetD))%33 + 64);//Impresión del nuevo string ASCII a través del ALGORITMO
+        } else {
+          decifrado += String.fromCharCode((positionAsciiDecode - 32 - parseInt(offSetD))%33 + 32);
+        }
+      }
+    }
+    return decifrado
+   }
 };
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
 import cipher from './cipher.js';
-
 /*console.log(cipher);*/
-
 let string1 = document.getElementById('string1');
 let string2 = document.getElementById('string2');
 let click1 = document.getElementById('click1');
 let click2 = document.getElementById('click2');
 let offSet1 = document.getElementById('offset1');
 let offSet2 = document.getElementById('offset2');
-
-
 click1.addEventListener('click',() => {
   let palabra1 = string1.value;
   //console.log(palabra1);
   let offSetC = offSet1.value;
-  string2.value = cipher.encode(palabra1,offSetC);
+  string2.value = cipher.encode(offSetC,palabra1);
 });  
-
-
 click2.addEventListener('click',() =>
 {
   let palabra2 = string2.value;
   let offSetD = offSet2.value;
-  string1.value = cipher.decode(palabra2,offSetD);
+  string1.value = cipher.decode(offSetD,palabra2);
 }); 
-
-
-
 /*else if(positionAsciiEncode === 33){
       resultado += '!';
       string2.value = resultado;
